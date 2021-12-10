686baa2309deb4533742a6cf729da90d87311685
Adrián Wohlmuth <adrianwohlmuth@gmail.com>
2021-12-10 11:55:11 +0100

    doplnenie cyklu while

--- 06-kalkulacka2/Source.cpp
+++ 06-kalkulacka2/Source.cpp
@@ -3,14 +3,16 @@ using namespace std;
 
 int main()
 {
-    char operacia;
+    char operacia, pokracovanie;
     float cislo1, cislo2;
 
-    cout << "Zadaj priklad napr. 10 + 5" << endl;
-    cin >> cislo1 >> operacia >> cislo2;
-
-    switch (operacia)
+    do
     {
+        cout << "Zadaj priklad napr. 10 + 5" << endl;
+        cin >> cislo1 >> operacia >> cislo2;
+
+        switch (operacia)
+        {
         case '+':
             cout << cislo1 << " + " << cislo2 << " = " << cislo1 + cislo2;
             break;
@@ -24,13 +26,16 @@ int main()
             break;
 
         case '/':
-            cout << cislo1 << " / " << cislo2 << " = " << cislo1 / cislo2;
+            cout << cislo1 << " / " << cislo2 << " = " << cislo1 / cislo2 << endl;
             break;
 
         default:
             cout << "Zly operator!!!";
             break;
-    }
+        }
+        cout << "Chces pokracovat ? (a/n)" << endl;
+        cin >> pokracovanie;
+    } while (pokracovanie == 'a');
 
     return 0;
 }
